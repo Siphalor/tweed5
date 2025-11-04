e7764585eb1225a9d04f4959e304fe3ae97b87c8
Siphalor <info@siphalor.de>
2025-11-04 22:30:20 +0100

    fix: Make publish-all script compatible with Bourne Shell

--- publish-all.sh
+++ publish-all.sh
@@ -1,6 +1,8 @@
-pushd tweed5 || exit
+#!/usr/bin/env sh
+
+cd tweed5 || exit
 ../gradlew publish
-popd || exit
-pushd tweed5-minecraft || exit
+cd .. || exit
+cd tweed5-minecraft || exit
 ./helpers/run-each-mc.sh ../gradlew publish
-popd || exit
+cd .. || exit
