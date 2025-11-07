0ee706876ee99f90e3b9f0192460786230202570
Siphalor <info@siphalor.de>
2025-11-08 00:48:45 +0100

    build(bundle, bundle-pojo-weaving): Remove obsolete sources jars

--- tweed5-minecraft/bundle-pojo-weaving/build.gradle.kts
+++ tweed5-minecraft/bundle-pojo-weaving/build.gradle.kts
@@ -1,18 +1,9 @@
-import de.siphalor.tweed5.minecraft.bundled.sources.BundledSourcesJar
-
 plugins {
 	id("de.siphalor.tweed5.minecraft.mod.bundle")
 }
 
 configurations.minecraftJijElements {
 	isTransitive = false
-}
-val bundledSourcesConfiguration = configurations.resolvable("bundledSources") {
-	extendsFrom(configurations.minecraftJijElements.get())
-	isTransitive = false
-	attributes {
-		attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType.SOURCES))
-	}
 }
 
 dependencies {
@@ -24,9 +15,4 @@ dependencies {
 	minecraftJij("de.siphalor.tweed5:tweed5-weaver-pojo-presets-extension")
 	minecraftJij("de.siphalor.tweed5:tweed5-weaver-pojo-serde-extension")
 	minecraftJij("de.siphalor.tweed5:tweed5-weaver-pojo-validation-extension")
-}
-
-tasks.register<BundledSourcesJar>("sourcesJar") {
-	sources.from(bundledSourcesConfiguration)
-	archiveClassifier.set("sources")
 }
--- tweed5-minecraft/bundle/build.gradle.kts
+++ tweed5-minecraft/bundle/build.gradle.kts
@@ -1,15 +1,5 @@
-import de.siphalor.tweed5.minecraft.bundled.sources.BundledSourcesJar
-
 plugins {
 	id("de.siphalor.tweed5.minecraft.mod.bundle")
-}
-
-val bundledSourcesConfiguration = configurations.resolvable("bundledSources") {
-	extendsFrom(configurations.implementation.get())
-	isTransitive = true
-	attributes {
-		attribute(DocsType.DOCS_TYPE_ATTRIBUTE, objects.named(DocsType.SOURCES))
-	}
 }
 
 dependencies {
@@ -18,9 +8,4 @@ dependencies {
 	minecraftJij("de.siphalor.tweed5:tweed5-default-extensions")
 	minecraftJij("de.siphalor.tweed5:tweed5-serde-extension")
 	minecraftJij(project(":tweed5-logging"))
-}
-
-tasks.register<BundledSourcesJar>("sourcesJar") {
-	sources.from(bundledSourcesConfiguration)
-	archiveClassifier.set("sources")
 }
